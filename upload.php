711c0c9d1334b0ea53fa0445fcc75cd5c48e49f1
yeong <yeonghak2207@gmail.com>
2025-01-25 20:58:36 +0900

    Second Commit and Push

--- upload.php
+++ upload.php
@@ -14,6 +14,22 @@ $path = "./upload/";
 // 업로드된 파일의 이름을 가져옴
 $filename = $_FILES["userfile"]["name"];
 
+//블랙리스트 방식의 확장자 검증 로직
+// 파일의 경로와 이름을 이용하여 파일 정보를 가져온다.
+$file_info = pathinfo($path.$filename);
+// 파일의 확장자를 추출하고 소문자로 변환합니다.
+$ext = strtolower($file_info["extension"]);
+// 블랙리스트 확장자 정의
+$ext_arr = array("php", "php3", "php5", "html", "htm");
+// 파일의 확장자가 허용되지 않는 목록에 포함되어 있는지 확인
+if(in_array($ext, $ext_arr)) {
+    echo "<script>alert('허용되지 않은 확장자 입니다!');history.back(-1);</script>";
+    exit();
+}
+
+
+
+
 // move_uploaded_file 함수를 사용하여 임시 저장된 파일을 지정된 위치로 이동
 // 이동 실패시 에러 메시지 출력
 if(!move_uploaded_file($_FILES["userfile"]["tmp_name"], $path.$filename)) {
