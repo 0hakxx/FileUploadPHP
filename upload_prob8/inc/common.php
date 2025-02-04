af80beadd36fbd3d48d2eaa5a309f076651a68d1
yeong <yeonghak2207@gmail.com>
2025-02-04 14:45:25 +0900

    9th Commit PUsh

--- upload_prob8/inc/common.php
+++ upload_prob8/inc/common.php
@@ -1,7 +1,7 @@
 <?
     $db_host = "localhost";
     $db_id = "root";
-    $db_pw = "crehacktive";
+    $db_pw = "123456";
     $db_name = "pentest";
 
     $db_conn = new mysqli($db_host, $db_id, $db_pw, $db_name);
